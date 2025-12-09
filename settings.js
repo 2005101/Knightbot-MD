e620c5b1718900a72bb31be3aba9d003d905b640
SKYPER MD <a.beeygutsa@gmail.com>
2025-12-09 13:36:21 +0200

    Update bot settings for packname and owner details

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'SKYPER-TECH MD,
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "SKYPER MD",
+  botOwner: 'BLAQ BOY', // Your name
+  ownerNumber: '263789670861', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
